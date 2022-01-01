bd0294b118421e49fa97087730fdc2e0b9504394
RAWANTEAM <96953906+RAWANTEAM@users.noreply.github.com>
2022-01-01 21:38:17 +0300

    Add files via upload

--- RAWAN.lua
+++ RAWAN.lua
@@ -9908,7 +9908,7 @@ infovid = "⌯︙ اختر صيغه التنزيل الان.\n"
 keyboard = {} 
 keyboard.inline_keyboard = {
 {{text = 'Mp4', callback_data=id_from_user..":DownloadVid:"..msgidrp..":"..id_from_vid..":Mp4"},{text = 'mp3', callback_data=id_from_user..":DownloadVid:"..msgidrp..":"..id_from_vid..":mp3"},{text = 'ogg', callback_data=id_from_user..":DownloadVid:"..msgidrp..":"..id_from_vid..":ogg"}},
-{{text = '𝚂𝙾𝚄𝚁𝙲𝙴ʀᴀᴡᴀɴ',url='http://t.me/CARLOSTEAMM'}},
+{{text = '𝚂𝙾𝚄𝚁𝙲𝙴ʀᴀᴡᴀɴ',url='http://t.me/VIP_FILES1'}},
 }
 https.request("https://api.telegram.org/bot"..token..'/editMessageText?chat_id='..Chat_id..'&text='..URL.escape(infovid)..'&message_id='..msg_idd..'&parse_mode=markdown&disable_web_page_preview=true&reply_markup='..JSON.encode(keyboard)) 
 InfoVid = https.request('https://black-source.tk/Api/BotYoutube.php?Id='..URL.escape(id_from_vid))
